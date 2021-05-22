7dfa2842b293fa8ff3a844b0685765d1742caee6
Robson Cristian de Lira <robso.crlira@gmail.com>
2021-05-22 07:00:33 -0300

    Criação do index para consultar na api da marvel

---
+++ backend/src/api-marvel/index.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const marvelGet = axios.create({
+    baseURL: 'http://gateway.marvel.com/v1/public/'
+});
+
+marvelGet.interceptors.request.use(config => {
+    config.params = config.params || {};
+    config.params.apikey = 'dbd0ea28d121f8376b8ff290f363fa76';
+    config.params.hash = 'c81f726f2a4e09fdb724ba1035de0e83';
+    config.params.ts = 'robson';
+    return config;
+});
+
+export { marvelGet }
