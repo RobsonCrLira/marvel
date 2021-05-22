18590ae522ea9a95c5478bbfda79e49518741133
Robson Cristian de Lira <robso.crlira@gmail.com>
2021-05-22 12:58:50 -0300

    criação de uma rota para charcters para testes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { ComicsConsulta } from './comics-consulta/comics';
+import { CharactersConsulta } from './characters-consulta/characters';
 
 const comicsget = new ComicsConsulta();
+const charactersget = new CharactersConsulta();
 const router = Router();
 
-router.get('/', comicsget.execute);
+router.get('/comics', comicsget.execute);
+router.get('/characters', charactersget.execute);
 
 export default router;
